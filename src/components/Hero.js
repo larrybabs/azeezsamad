c1d4433e960a4c5adbf713757feee4cc1b289130
Azeez Samad <seshp@ymail.com>
2022-04-29 20:08:47 +0100

    change font size

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,8 +9,8 @@ const Hero = () => {
 
         <div className="container max-w-[1000px] mx-auto px-6 flex flex-col justify-center h-full">
             <p className="text-purple-600">Hello, my name is</p>
-            <h1 className="text-6xl sm:text-7xl font-bold text-[#ccd6f6]"> Azeez Samad</h1>
-            <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]"> I'm a Front-end developer and a UI/UX <span className="text-purple-600 sm:text-8xl">designer.</span> </h2>
+            <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]"> Azeez Samad</h1>
+            <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]"> I'm a Front-end developer and a UI/UX <span className="text-purple-600">designer</span> </h2>
             <p className="text-[#8892b0] py-4 max-w-[700px]"> I’m a front-end developer specializing in building (and occasionally
           designing) exceptional digital experiences. Currently, I’m focused on
           building responsive web applications.</p>
