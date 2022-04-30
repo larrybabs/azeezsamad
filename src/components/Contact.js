9e7443ac587ea858e61bc4e589819e2205f5b785
Azeez Samad <seshp@ymail.com>
2022-04-30 18:48:08 +0100

    profile links added to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {
+  FaGithub,
+  FaLinkedin,
+  FaBehance,
+} from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const Contact = () => {
   return (
-    <div  name="contact" className="w-full h-screen bg-[#1b1188] flex justify-center items-center p-4">
+    <div  name="contact" className="w-full h-screen bg-[#1b1188] flex justify-center flex-col items-center p-4">
       <form method="POST" action="https://getform.io/f/00f5d63f-97e3-4e26-82c3-4d3126d229a4" className="flex flex-col max-w-[600px] w-full">
         <div className="pb-8">
             <p className="text-4xl inline border-b-4 border-purple-600 font-bold text-gray-300">Contact Me </p>
@@ -13,6 +19,42 @@ const Contact = () => {
         <textarea className="p-2 outline-none" name="message" rows="10" placeholder="Enter message"></textarea>
         <button className="py-3 px-4 my-8 mx-auto text-white border-2 flex items-center hover:bg-purple-600 hover:border-purple-600">Let's Work</button>
       </form>
+      <div className="">
+      <ul className="flex lg:hidden justify-between mt-5 text-white">
+        <li>
+            <a
+              href="https://www.linkedin.com/in/samad-azeez-656b6621a/"
+            >
+              <FaLinkedin size="30" />
+            </a>
+          </li>
+          <li >
+            <a
+              href="https://github.com/larrybabs"
+            >
+              <FaGithub size="30" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.behance.net/samadolanrewaju"
+              
+            >
+              <FaBehance size="30" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.notion.so/Professional-Resume-6ed0ad28ff1246399e8d6678e9dbb3d2"
+            >
+               <BsFillPersonLinesFill size="30" />
+            </a>
+          </li>
+        </ul>
+        <div class="text-white mt-3 lg:mt-10">
+                Copyright &copy; 2022 Samad Azeez
+            </div>
+      </div>
     </div>
   );
 }
